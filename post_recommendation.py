ed2066f9421a2425a6f39f9e61dc39ec3b0f6079
Abhay Bhaskar <99061771+StructuredProgramming@users.noreply.github.com>
2023-10-14 11:48:30 -0400

    Update post_recommendation.py

--- post_recommendation.py
+++ post_recommendation.py
@@ -9,11 +9,8 @@ posts_visited = [
 
 user_posts = [" ".join([title, content]) for (title, content) in posts_visited]
 
-# Titles and content of all posts
+# Titles and content of all posts excluding posts already viewed
 total_posts = [
-    ("Integration", "Integration is an important part of calculus, forming a strong foundation for the subject with a variety of intricacies."),
-    ("Coffee","Coffee is a very good liquid for consumption, providing lots of energy at a relatively low cost."),
-    ("Trains","I like trains."),
     ("Topelitz Square", "The topelitz square is one of the most difficult problems in mathematics, with a variety of interesting proposed solutions."),     
     ("Cancer Research", "Cancer is one of the most deadly diseases in the world, but thanks to the work of incredible doctors there a wide variety of available treatments."),
     ("Phineas and Ferb", "Phineas and Ferb is the greatest TV show of all time, with a lot of interesting storylines.")]
@@ -22,16 +19,18 @@ all_posts = [" ".join([title, content]) for (title, content) in total_posts]
 
 # Create TfidfVectorizer to convert text to numerical vectors
 vectorizer = TfidfVectorizer(stop_words='english')
-post_vectors = vectorizer.fit_transform(all_posts)
+post_vectors = vectorizer.fit_transform(all_posts + user_posts)  # Include user posts for vectorization
 
 # Compute cosine similarity between user posts and all posts
 user_post_vectors = vectorizer.transform(user_posts)
-cosine_similarities = cosine_similarity(user_post_vectors, post_vectors)
+cosine_similarities = cosine_similarity(user_post_vectors, post_vectors[:-len(user_posts)])  # Exclude user posts for similarity calculation
 
-# Get indices of recommended posts (excluding already seen posts)
-recommended_post_indices = cosine_similarities.argsort()[0][::-1]
-filtered_indices = [i for i in recommended_post_indices if i < len(posts_visited) and i not in user_posts]
-recommended_posts = [(posts_visited[i][0], posts_visited[i][1]) for i in filtered_indices if i not in user_posts]
+# Get indices of all posts and sort them based on similarity
+all_post_indices = range(len(total_posts))
+sorted_indices = sorted(all_post_indices, key=lambda x: cosine_similarities[0][x], reverse=True)
+
+# Get recommended posts based on sorted indices
+recommended_posts = [(total_posts[i][0], total_posts[i][1]) for i in sorted_indices]
 
 # Print recommended posts
 print("Recommended Posts:")
